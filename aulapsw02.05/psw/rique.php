302907a069bc20b6326ebecc47515491213534c6
rique pimentel <130499366+HenriquePCA@users.noreply.github.com>
2023-09-19 14:25:16 -0300

    Add files via upload

---
+++ aulapsw02.05/psw/rique.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ARRAYS</title>
+</head>
+<body>
+    <h1>ARRAYS</h1>
+    <?php 
+        
+        $vetor = [
+            'nome' => 'GUSTAVO',
+            'endereco' => 'AV. GUANABARA',
+            'cidade' => 'GUANAMBI'
+        ];
+
+        echo $vetor['nome'];
+      
+
+    ?>
+</body>
+</html>
---
+++ aulapsw02.05/psw2/rique.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ARRAYS</title>
+</head>
+<body>
+    
+<?php
+
+$vetor = [
+'nome' => 'HENRIQUE',
+'local' => 'AVN. BRINDES',
+'idade' => '17',
+
+];
+
+echo $vetor['nome'];
+echo '<br>';
+echo $vetor['local'];
+echo '<br>';
+echo $vetor['idade'];
+
+
+?>
+
+</body>
+</html>
